8a7eb0dae17f07b0ddee1b459075aa87e0438c42
parasuram423 <parasurams423@gmail.com>
2025-06-27 08:10:14 +0530

    Update Course.js

--- Course.js
+++ Course.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
 const courseSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    category: String,
-    duration: String,
-    link: String
+  title: String,
+  description: String,
+  instructor: String,
+  duration: String
 });
 
 module.exports = mongoose.model('Course', courseSchema);
